b7cdf28887d3faea9f18a48ed418b0f346d58d62
hannah091 <hannah.or91@gmail.com>
2024-08-14 09:27:34 +1000

    Our team styled headings
    Our team styled headings

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -8,8 +8,8 @@ function Team(){
 <div className="wrapper">
       
 
-  <div className="heading">
-    <h1>Our Dentists</h1>
+  <div className="dentist-heading">
+    <h1 className='text-5xl font-bold mb-8'>Our Dentists</h1>
     <p>Our team at North Road Dental Clinic comprise of highly-trained, experienced and friendly dental professionals, committed to ensuring you receive the best appropriate dental care for your needs.</p>
   </div> 
 
@@ -19,9 +19,9 @@ function Team(){
     </div>
 
     <div className="text">
-      <h2> Dr. Rose Gulnaz Cemiloglu </h2> 
-      <h3>Principal Dentist,  Female, Bachelor of Dental Surgery (BDS)</h3>
-      <h3>Speaks English, and Turkish</h3>
+      <h2 className="dentist-name-heading"> Dr. Rose Gulnaz Cemiloglu </h2> 
+      <h3 className="Dentist-subheading">Principal Dentist,  Female, Bachelor of Dental Surgery (BDS)</h3>
+      <h3 className="Dentist-subheading">Speaks English, and Turkish</h3>
     
       <p>Dr. Rose brings her extensive experience and skills gleaned over 29 years to North Road Dental Clinic. She is a member of ADA and ADAVB. She enjoys all aspects of general and family dentistry, including cosmetic dentistry. 
         She has a special interest in endodontics, prosthodontics, periodontology, and recently implantology. She will be privileged to help you or your loved ones on any aspect of their dental health.
@@ -41,9 +41,9 @@ function Team(){
     </div>
 
     <div className="text">
-      <h2> Dr. Fabiola Menegat </h2> 
-      <h3>Dentist, Female, Bachelor of Dental Surgery (BDS)</h3>  
-      <h3>Speaks English, and Portuguese</h3>
+      <h2 className="dentist-name-heading"> Dr. Fabiola Menegat </h2> 
+      <h3 className="Dentist-subheading">Dentist, Female, Bachelor of Dental Surgery (BDS)</h3>  
+      <h3 className="Dentist-subheading">Speaks English, and Portuguese</h3>
     
       <p>Dr. Fabiola graduated in 2003 with a Bachelor of Dental Science(Hons), and since then has worked as a dentist in public and private clinics, both in Australia and overseas. She is a gentle and caring dentist and enjoys working with adults and children. Dr. Fabiola is passionate to provide a positive experience to patients and has a strong focus in preventative dentistry to help patients maintain healthy teeth and gums long term. Outside of work, she likes meeting with friends, cooking and yoga.</p>
     </div>
@@ -56,9 +56,9 @@ function Team(){
     </div>
 
     <div className="text">
-      <h2>Dr. Aliakbar Safidroodi</h2> 
-      <h3>Dentist, Male, Bachelor of Dental Surgery (BDS)</h3>  
-      <h3>Speaks English</h3>
+      <h2 className="dentist-name-heading">Dr. Aliakbar Safidroodi</h2> 
+      <h3 className="Dentist-subheading">Dentist, Male, Bachelor of Dental Surgery (BDS)</h3>  
+      <h3 className="Dentist-subheading">Speaks English</h3>
     
       <p>Dr Ali brings his extensive knowledge on dental science spanning 2 decades. He enjoys treating patients of all ages and needs, and has a special interest in oral surgery and implantology. Dr Ali is our clinic's visiting surgeon for our implant patients.</p>
     </div>
